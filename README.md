1132c74bfaecdd09576f6b7c33e471117583372b
PabloHiro <PabloHiro@users.noreply.github.com>
2023-03-08 19:37:50 +0000

    jekyll build from Action 80306c88554421dcb6ff9ec3e13cb380dd4789f4

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on: https://github.com/mmistakes/mm-github-pages-starter
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Github Pages and Jekyll",
+        "excerpt":"I have created my personal webpage using Github Actions and Github Pages. Github allows its users to publish their webpages for free using the Github Pages feature. Github Pages simply serves the contents of a repository branch as a webpage, hence assuming there is an index.html file that serves as...","categories": [],
+        "tags": [],
+        "url": "/page/2022/12/25/hello-world.html",
+        "teaser": null
+      },{
+        "title": "Dictionaries and lists in Ansible",
+        "excerpt":"Today I had the chance to explain to a colleague how dictionaries and lists are defined in Ansible. The information in this post can be useful to understand how Python, JSON and YAML handle data structures, as some of the patterns shown here are used by these tools. I will...","categories": [],
+        "tags": [],
+        "url": "/page/2022/12/28/dictionaries-and-lists.html",
+        "teaser": null
+      },{
+        "title": "Backups",
+        "excerpt":"I will be explaining the difference between a Full backup, a Differential backup and an Incremental backup. It is important to understand these three concepts in order to plan the most suitable backup strategy and be able to predict the storage required by a backup. A full backup performs a...","categories": [],
+        "tags": [],
+        "url": "/page/2023/01/19/backups.html",
+        "teaser": null
+      }]
