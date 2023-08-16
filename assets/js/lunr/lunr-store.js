4c4ba47c1a82926d407ca435de7b9b3c7b7fcdbd
PabloHiro <PabloHiro@users.noreply.github.com>
2023-08-16 13:52:36 +0000

    jekyll build from Action 35aa70c917a876db8ec66de129ca0bda8d3bfd33

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Github Pages and Jekyll",
+        "excerpt":"I have created my personal webpage using Github Actions and Github Pages. Github allows its users to publish their webpages for free using the Github Pages feature. Github Pages simply serves the contents of a repository branch as a webpage, hence assuming there is an index.html file that serves as...","categories": [],
+        "tags": [],
+        "url": "/page/2022/12/25/hello-world.html",
+        "teaser": null
+      },{
+        "title": "Dictionaries and lists in Ansible",
+        "excerpt":"Today I had the chance to explain to a colleague how dictionaries and lists are defined in Ansible. The information in this post can be useful to understand how Python, JSON and YAML handle data structures, as some of the patterns shown here are used by these tools. I will...","categories": [],
+        "tags": [],
+        "url": "/page/2022/12/28/dictionaries-and-lists.html",
+        "teaser": null
+      },{
+        "title": "Backups",
+        "excerpt":"I will be explaining the difference between a Full backup, a Differential backup and an Incremental backup. It is important to understand these three concepts in order to plan the most suitable backup strategy and be able to predict the storage required by a backup. A full backup performs a...","categories": [],
+        "tags": [],
+        "url": "/page/2023/01/19/backups.html",
+        "teaser": null
+      },{
+        "title": "Linux and Windows with the same keyboard and mouse",
+        "excerpt":"I have recently refurbished an old computer I had at my parents place and decided it was time to have my first Linux physical machine and not just a virtual machine. However, I had to solve the problem of having only a single keyboard and mouse at home and avoid...","categories": [],
+        "tags": [],
+        "url": "/page/2023/08/03/barrier.html",
+        "teaser": null
+      }]
